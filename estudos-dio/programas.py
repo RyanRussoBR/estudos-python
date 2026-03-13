d4f0be65a17dd6bae3271c62100b68012f6e168e
Ryan Russo <ryangoreckirusso12@gmail.com>
2026-03-13 18:50:04 -0300

    Estudos DIO

--- estudos-dio/programas.py
+++ estudos-dio/programas.py
@@ -1,1 +1,67 @@
 # print('Hello World!, Vou aprender python e entrar no mercado ihuu') #primeiro programa do dio :)
+
+
+
+# Aula de Variáveis e Constantes
+
+# Quando for atribuir nome para constantes, sempre colocar o nome em MAIÚSCULO, pois o Python não sabe diferenciar uma variável de uma constante (nenhuma linguagem na vdd huashudsahuha)
+
+# nome = 'Ryan'
+# idade = 20
+# nome, idade = ("Ryan", 20) -> Outra forma
+# print(nome, idade)
+
+# limiteSaque_diario = 1000 # Esse tipo de nome ficou estiloso
+
+
+# BRAZILIAN_STATES = [ "SP", "RJ", "PR", "AL"] # Esse é uma constante, nome somente com letras maíusculas, letra até fica diferente uwu
+
+# # BRAZILIAN_STATES = 10 #Porém se tentar alterá-lo, ele irá pois o Python não identifica automaticamente como constante, tudo pra ele é variável
+
+# print(BRAZILIAN_STATES)
+
+
+
+# Aula de conversão de tipos.
+
+# print(int('10')+5) #Não precisa fazer isso, somente teste.
+
+# preco = 10
+# # print(preco)
+
+# # preco = float(preco)
+# # print(preco)
+
+# # Divisão retorna resultado com ponto flutuante, mesmo se o resultado de um número inteiro
+# print(preco/2)
+
+# # E para tirar o ponto flutuante só converter para int o valor.
+# print(int(preco/2))
+
+
+
+# Aula de funções de entrada e saída
+
+# nome = input('Qual o nome -> ')
+# print('Olá',nome,'-','Seja bem vindo(a)') 
+
+# Print com argumentos opcionais
+
+# nome = 'Ryan'
+# sobrenome = 'Russo'
+
+# print(nome, sobrenome)
+# print(nome, sobrenome, end='...\n') #O end coloca o que está nas aspas no final do print
+# print(nome, sobrenome, sep='#')#Já o sep coloca o que está nas aspas no espaço que o print terá
+
+# nome = input('Qual o seu nome? ')
+# sobrenome = input('Qual seu sobrenome? ')
+
+# print(nome, sobrenome)
+# print(nome, sobrenome, end='...\n')
+# print(nome, sobrenome, end='...\n', sep='$')
+# print(nome, sobrenome, sep='$')
+
+
+#Tive a impressão de que o end além de adicionar coisas no final, ele "junta" dois prints na mesma linha
+
