141dfeb589ee14b3d1733bcf4127990d8836aee2
Ryan Russo <ryangoreckirusso12@gmail.com>
2026-03-19 23:30:18 -0300

    More studies

--- estudos-dio/programas.py
+++ estudos-dio/programas.py
@@ -65,3 +65,106 @@
 
 #Tive a impressão de que o end além de adicionar coisas no final, ele "junta" dois prints na mesma linha
 
+
+
+# Operadores aritméticos
+# print(12/2) #Divisão
+# print(12//3) #Divisão por inteiro
+
+# print(10%3) # Módulo (quantos 3 cabem dentro de 10)
+# print(2**3) # Exponenciação
+
+
+# Ordem de precedência
+# x = 10 / 2 * 4
+# print(x)
+# Quest: Resultado vai dar 0 ou 10
+# ans: Acho que vai dar 0 segunda a ordem de precedência. CORRECT
+
+# A precedência ocorre na seguinte ordem:
+# Parênteses -> Expoentes (Exponenciação) -> Multiplicação e Divisão (da esquerda para direita, desde que sem parênteses) -> Adição e Subtração (da esquerda para direita, desde que sem parênteses) 
+
+# Operadores de comparação (para comparar dois valores) E sempre retorna um valor booleano (True ou False):
+# Igualdade (==)
+# Maior / Maior ou igual (>=) 
+# Menor / Menor ou igual (<=) parece uma flecha bem legal
+# saldo = 600
+# saque = 500
+# print(saldo < saque)
+# UM DOS MAIS IMPORTANTES NO CÓDIGO
+
+# Operadores de atribuição 
+# Aqui é o sinal de = (igual), mas como vi na aula de lógica de programação, mudei meu pensamento, não chamo mais sinal de igual, mas sim de atribuição (etiqueta)
+# Também consigo fazer adição na atribuição com +=
+# Já para subtrair fica -=
+# Com multiplicação *=
+# Com divisão /= e Divisão inteira //=
+# Com módulo %= e com Exponenciação **=
+# saldo = 500
+# print(saldo)
+# saldo //= 200
+# print(saldo)
+
+# Operadores lógicos
+# É uma operação que geralmente está junto com os operadores de comparação que "concatena" tudo como uma grande expressão lógica
+# Operador e (and) - Para ser True, os dois (ou todas as comparações) devem ser True
+# Operador ou (or) - Para ser True, basta uma das comparações ser True, já para ser False todos devem ser False
+# Operador Negação (not) - O contrário da comparação, se for um True, vai resultar em False, pode negar tanto sequências de caracteres (strings) e objetos (listas)
+# Os operadores lógicos e de comparação sempre trabalham juntinhos, olha que fofo!
+# saldo = 1000
+# saque = 200
+# limite = 100
+
+# print(not saldo >= saque or saque <= limite)
+
+# Interessante usar parêtenses se o operador lógico ser muito extenso, seguindo o exemplo:
+
+# saldo = 1000
+# saque = 200
+# limite = 100
+# conta_especial = True
+
+# print((saldo >= saque and saque <= limite) or (conta_especial and saldo >= saque))
+
+# Dica: As comparações são melhores guardarem em variáveis para que o código fique mais legível
+# saldo = 1000
+# saque = 200
+# limite = 100
+# conta_especial = True
+
+# conta_com_saldoSuficiente = saldo > saque and saque <= limite
+# conta_especial_com_saldoSuficiente = conta_especial and saldo >= saque
+
+# exp2 = conta_com_saldoSuficiente or conta_especial_com_saldoSuficiente
+# print(exp2) # Caraca fica bem melhor
+ 
+# Operadores de identidade: Ele comparar se dois objetos ocupam mesmo espaço na memória
+# Operador Is, ele verifica se o objeto em questão ocupa a mesma posição de outro objeto
+# Operador Is not, é somente a negação como todo o not :)
+# Operador is instance, verifica o tipo de dado que uma variável é, se é int, str, etc. # esse peguei porque sou curioso kkkkkk
+# Funciona tanto com strings quanto com numeros
+# curso = 'Curso de Python'
+# nome_curso = curso
+# saldo, limite = 200, 200
+
+# # print(saldo is limite)
+# saldo, limite = 'Alo', 1000
+# print(isinstance(saldo, str))
+
+
+# Operadores de Associação: Verifica se algum objeto pertence a uma sequência
+# Operador in
+# Operador not in (contrário)
+# Muito utilizado, principalmente em banco de dados
+# frutas = [
+#     "melancia",
+#     "laranja",
+#     "uva"
+# ]
+
+# print("uva" in frutas)
+
+# curso = ('Curso de Python')
+
+# print('python' not in curso)
+
