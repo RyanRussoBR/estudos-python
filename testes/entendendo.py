e0201e5917cee0b149a841d903129a40a944d87d
Ryan Russo <ryangoreckirusso12@gmail.com>
2026-05-05 15:17:53 -0300

    estudos

---
+++ testes/entendendo.py
@@ -0,0 +1,87 @@
+# Entendo as transações da conta. BLOCO 1
+# class Conta:
+#     def __init__(self):
+#         self.saldo = 0
+    
+#     def sacar(self, valor):
+#         valor = float(input('Qual valor sacar? -> R$'))
+#         if valor <= self.saldo:
+#             self.saldo -= valor
+#         else:
+#             print('Saldo insuficiente.')
+        
+#     def depositar(self, valor):
+#         valor = float(input('Qual valor depositar? -> R$'))
+#         self.saldo += valor
+
+#     def __str__(self):
+#         return f"{self.saldo}"
+
+# conta = Conta()
+# conta.depositar(1)
+# conta.sacar(1)
+# print(conta)
+
+# Jeito um pouco diferente, mas gosto de interatividade com o usuário :)
+
+# BLOCO 2 - Histórico de transações.
+# class Conta:
+#     def __init__(self):
+#         self.saldo = 0
+    
+#     def sacar(self, valor):
+#         valor = float(input('Qual valor sacar? -> R$'))
+#         if valor <= self.saldo:
+#             self.saldo -= valor
+#         else:
+#             print('Saldo insuficiente.')
+        
+#     def depositar(self, valor):
+#         valor = float(input('Qual valor depositar? -> R$'))
+#         self.saldo += valor
+
+#     def __str__(self):
+#         return f"{self.saldo}"
+    
+# class Historico:
+#     def __init__(self):
+#         self.transacoes = []
+
+#     def adicionar(self, transacao):
+#         self.transacoes.append({
+#             "tipo": transacao.__class__.__name__,
+#             "valor": transacao.valor
+#         })
+    
+#     def __str__(self):
+#         return f"{self.transacoes}"
+    
+# class Deposito:
+#     def __init__(self, valor):
+#         self.valor = valor
+#         pass
+    
+
+# class Saque:
+#     def __init__(self, valor):
+#         self.valor = valor
+#         pass
+
+# historico = Historico()
+# s = Saque(100)
+# d = Deposito(100)
+# # conta = Conta()
+# # conta.depositar(1)
+# # historico.adicionar()
+# # conta.sacar(1)
+# # historico.adicionar()
+# # print(conta)
+# # print(historico)
+# historico.adicionar(d)
+# historico.adicionar(s)
+# print(historico)
+
+# As classes Deposito e Saque são feitas para representar o tipo e o valor feito na operação realizada pelo usuário.
+
+
+        
